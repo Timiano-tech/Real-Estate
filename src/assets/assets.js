a840bae03ce2462a697c4728555b73b1feeb5d93
Timiano <timiajayi729@gmail.com>
2025-12-24 10:55:07 +0100

    Complete the Navigation Bar and The Header page

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -5,8 +5,8 @@ import menu_icon from './menu_icon.svg'
 import star_icon from './star_icon.svg'
 import left_arrow from './left_arrow.svg'
 import right_arrow from './right_arrow.svg'
-import header_img from './header_img.png'
 import brand_img from './brand_img.png'
+import cross from './cross.png'
 import project_img_1 from './project_img_1.jpg'
 import project_img_2 from './project_img_2.jpg'
 import project_img_3 from './project_img_3.jpg'
@@ -21,9 +21,9 @@ export const assets = {
     logo,
     logo_dark,
     cross_icon,
+    cross,
     menu_icon,
     star_icon,
-    header_img,
     brand_img,
     project_img_1,
     project_img_2,
